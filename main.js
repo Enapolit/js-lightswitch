50076c12dd46a6afe2ca8cbdac27bb982d4d7d72
enapolit <elizabeth.napolitano61@journalism.cuny.edu>
2022-02-28 19:41:56 -0500

    enter

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 /**
  * SILENCE IS GOLDEN
  */
+
+//alert("Hello, World!");
+const body = document.querySelector("body");
+const lightswitches = document.querySelectorAll(".switch");
+
+lightswitches.forEach(function (lightswitch) {
+lightswitch.addEventListener("click", function (e) {
+    e.preventDefault(); // prevent the default behavior on the click
+    body.classList.toggle("light-off");
+});
+});
